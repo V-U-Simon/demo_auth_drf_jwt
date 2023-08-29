6f290852fd8107d06be6aa94c835d08587c9b993
V-U-Simon <V-U-Simon@yandex.ru>
2023-08-29 08:16:07 +0300

    back: user_app

--- back/config/settings.py
+++ back/config/settings.py
@@ -37,13 +37,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "rest_framework_simplejwt",
+    "user_app",
 ]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.DjangoModelPermissions"],
 }
 
 MIDDLEWARE = [
--- back/config/urls.py
+++ back/config/urls.py
@@ -1,20 +1,19 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
-
-urlpatterns += [
-    # get access and refresh tokens
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    # simple-JWT
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    # get new access token by refresh token
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    # check token validity
     path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    #
+    path("api/", include("user_app.router")),
 ]
